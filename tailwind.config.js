db56907dc6c3a9ba73cddaaec2b85036d63542c8
Haobo Lv <lvhaobo@foxmail.com>
2025-08-26 18:54:26 +0800

    refactor: 将配置文件从ESM迁移到CommonJS并更新tailwindcss
    移除@tailwindcss/postcss插件并降级tailwindcss版本以保持兼容性

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
@@ -103,4 +103,4 @@ export default {
     },
   },
   plugins: [],
-}
+};
